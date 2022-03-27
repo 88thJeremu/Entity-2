17fdb1062159831df9383b9422882f8a0a2282f4
7max18 <maxkoppel0703@gmail.com>
2022-03-26 22:39:42 -0400

    Implemented basic battle scene, initiative, and player/enemy stats

---
+++ Entity_2/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleManager : MonoBehaviour
+{
+    private List<Stats> combatants = new List<Stats>();
+    private int turnCounter = 0;
+
+    public GameObject p1Data;
+    public GameObject p2Data;
+    public GameObject eventText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetInitiative();
+
+        foreach (Stats combatant in combatants)
+        {
+            Debug.Log(combatant.characterName);
+        }
+
+        AdvanceTurn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetInitiative()
+    {
+        foreach (Transform combatant in GameObject.Find("Combatants").transform)
+        {
+            Stats m_stats = combatant.gameObject.GetComponent<Stats>();
+            
+            if (m_stats != null)
+            {
+                combatants.Add(m_stats);
+            }   
+        }
+
+        combatants.Sort(SortBySpeed);
+        combatants.Reverse();
+    }
+
+    void AdvanceTurn()
+    {
+        switch (combatants[turnCounter].characterName)
+        {
+            case "Player 1":
+                p1Data.SetActive(true);
+                p2Data.SetActive(false);
+                break;
+            case "Player 2":
+                p2Data.SetActive(true);
+                p1Data.SetActive(false);
+                break;
+            default:
+                p1Data.SetActive(false);
+                p2Data.SetActive(false);
+                break;
+        }
+
+        turnCounter++;
+        
+        if (turnCounter == combatants.Count)
+        {
+            turnCounter = 0;
+        }
+    }
+
+    static int SortBySpeed(Stats s1, Stats s2)
+    {
+        return s1.SPD.CompareTo(s2.SPD);
+    }
+}
---
+++ Entity_2/Assets/Scripts/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public string characterName;
+    public int HP; //Health
+    public int ATK; //Attack power; damage dealt
+    public int DEF; //Defense; subtracted from enemy ATK when attacked
+    public int SPD; //Speed; determines turn order
+    public Attack[] attacks;
+}
