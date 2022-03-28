7efd110818787aa9189df3552faf2ddd7a132444
7max18 <maxkoppel0703@gmail.com>
2022-03-27 23:16:26 -0400

    Finished battle system basics

--- Entity_2/Assets/Scripts/BattleManager.cs
+++ Entity_2/Assets/Scripts/BattleManager.cs
@@ -1,34 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
     private List<Stats> combatants = new List<Stats>();
     private int turnCounter = 0;
+    private bool pickingTarget = false;
+    private bool waitingForEnemyAttack = false;
+    private bool waitingForNextTurn = false;
+    private Stats currentAttacker;
+    private Stats currentTarget;
+    private Attack selectedAttack;
 
     public GameObject p1Data;
+    private Text p1HP;
     public GameObject p2Data;
-    public GameObject eventText;
+    private Text p2HP;
+    public Text eventText;
 
     // Start is called before the first frame update
     void Start()
     {
+        p1HP = p1Data.transform.GetChild(0).GetComponent<Text>();
+        p2HP = p2Data.transform.GetChild(0).GetComponent<Text>();
         SetInitiative();
-
-        foreach (Stats combatant in combatants)
-        {
-            Debug.Log(combatant.characterName);
-        }
-
         AdvanceTurn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (pickingTarget)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.CompareTag("Enemy"))
+                    {
+                        pickingTarget = false;
+                        currentTarget = hit.transform.gameObject.GetComponent<Stats>();
+                        ResolveAttack(currentAttacker, currentTarget);
+                    }
+                }
+            }
+            else if (waitingForEnemyAttack)
+            {
+                waitingForEnemyAttack = false;
+                ResolveAttack(currentAttacker, currentTarget);
+            }
+            else if (waitingForNextTurn)
+            {
+                waitingForNextTurn = false;
+                AdvanceTurn();
+            }
+        }
     }
 
     void SetInitiative()
@@ -36,11 +66,11 @@ public class BattleManager : MonoBehaviour
         foreach (Transform combatant in GameObject.Find("Combatants").transform)
         {
             Stats m_stats = combatant.gameObject.GetComponent<Stats>();
-            
+
             if (m_stats != null)
             {
                 combatants.Add(m_stats);
-            }   
+            }
         }
 
         combatants.Sort(SortBySpeed);
@@ -49,28 +79,118 @@ public class BattleManager : MonoBehaviour
 
     void AdvanceTurn()
     {
-        switch (combatants[turnCounter].characterName)
-        {
-            case "Player 1":
-                p1Data.SetActive(true);
-                p2Data.SetActive(false);
-                break;
-            case "Player 2":
-                p2Data.SetActive(true);
-                p1Data.SetActive(false);
-                break;
-            default:
-                p1Data.SetActive(false);
-                p2Data.SetActive(false);
-                break;
-        }
+        bool enemiesLeft = false;
 
-        turnCounter++;
-        
-        if (turnCounter == combatants.Count)
+        if (turnCounter >= combatants.Count)
         {
             turnCounter = 0;
         }
+
+        foreach(Stats combatant in combatants)
+        {
+            if (combatant.CompareTag("Enemy"))
+            {
+                enemiesLeft = true;
+            }
+        }
+
+        if (enemiesLeft)
+        {
+            currentAttacker = combatants[turnCounter];
+
+            switch (combatants[turnCounter].characterName)
+            {
+                case "Player 1":
+                    p1Data.SetActive(true);
+                    eventText.text = "What will Player 1 do?";
+                    break;
+                case "Player 2":
+                    p2Data.SetActive(true);
+                    eventText.text = "What will Player 2 do?";
+                    break;
+                default:
+                    PrepareEnemyAttack(combatants[turnCounter]);
+                    break;
+            }
+
+            turnCounter++;
+        }
+        else
+        {
+            eventText.text = "Battle clear!";
+        }
+    }
+
+    public void PreparePlayerAttack(Attack attack)
+    {
+        p1Data.SetActive(false);
+        p2Data.SetActive(false);
+        eventText.text = "Select a target.";
+        pickingTarget = true;
+        selectedAttack = attack;
+    }
+
+    private void PrepareEnemyAttack(Stats enemy)
+    {
+        Attack attack = enemy.attacks[Random.Range(0, enemy.attacks.Length)];
+        float targetRoll = Random.value;
+        Stats target;
+
+        if (targetRoll <= 0.5)
+        {
+            target = GameObject.Find("Player 1").GetComponent<Stats>();
+        }
+        else
+        {
+            target = GameObject.Find("Player 2").GetComponent<Stats>();
+        }
+
+        eventText.text = enemy.characterName + " attacked " + target.characterName + " with " + attack.attackName + "!";
+
+        currentAttacker = enemy;
+        currentTarget = target;
+        waitingForEnemyAttack = true;
+    }
+
+    void ResolveAttack(Stats damager, Stats damaged)
+    {
+        float hitRoll = Random.value;
+        if (hitRoll <= selectedAttack.hitChance)
+        {
+            eventText.text = "The attack hits!";
+            damaged.HP -= Mathf.RoundToInt(selectedAttack.attackStrength * damager.ATK) - damaged.DEF;
+
+            //Updates Player HP readings, if necessary
+            if (damaged.characterName == "Player 1")
+            {
+                p1HP.text = "HP: " + damaged.HP.ToString();
+            }
+            else if (damaged.characterName == "Player 2")
+            {
+                p2HP.text = "HP: " + damaged.HP.ToString();
+            }
+
+            if (damaged.HP <= 0)
+            {
+                damaged.HP = 0;
+                combatants.Remove(damaged);
+
+                if (damaged.gameObject.CompareTag("Enemy"))
+                {
+                    Destroy(damaged.gameObject);
+                }
+                else if (damaged.gameObject.CompareTag("Player"))
+                {
+                    eventText.text = damaged.characterName + " was overwhelmed!";
+                }
+            }
+        }
+        else
+        {
+            eventText.text = "The attack misses!";
+        }
+
+        waitingForNextTurn = true;
     }
 
     static int SortBySpeed(Stats s1, Stats s2)
---
+++ Entity_2/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject actionsList;
+    public GameObject attacksList;
+    public Stats partyMember;
+    public void OpenAttacks()
+    {
+        actionsList.SetActive(false);
+        attacksList.SetActive(true);
+    }
+    public void OpenActions()
+    {
+        actionsList.SetActive(true);
+        attacksList.SetActive(false);
+    }
+}
